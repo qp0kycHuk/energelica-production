df5b984e71dcb247879bbbeac240000ab00a2d44
qp0kycHuk <serega990112@mail.ru>
2024-11-05 15:36:35 +0300

    add started animation

--- src/js/animations.ts
+++ src/js/animations.ts
@@ -32,6 +32,7 @@ function init() {
   gsap.registerPlugin(ScrollTrigger)
 
   scrollInit()
+  hover3dInit()
 
   document.addEventListener('toggleopen', () => {
     ScrollTrigger.refresh()
@@ -100,6 +101,40 @@ function scrollInit() {
     instance.then(() => {
       gsap.set(element, {
         transform: 'none',
+      })
+    })
+  })
+}
+
+
+function hover3dInit() {
+  const limits = 15.0
+  const cards = document.querySelectorAll('.hover3d-wrap')
+
+  cards.forEach((card) => {
+    const item = card.querySelector('.hover3d-target')
+
+    card.addEventListener('mousemove', (e) => {
+      const event = e as MouseEvent
+      const rect = (event.target as HTMLElement)?.getBoundingClientRect()
+      const x = event.clientX - rect.left
+      const y = event.clientY - rect.top
+      const offsetX = x / rect.width
+      const offsetY = y / rect.height
+
+      const rotateY = offsetX * (limits * 2) - limits
+      const rotateX = offsetY * (limits * 2) - limits
+
+      gsap.to(item, {
+        rotateX: -rotateX,
+        rotateY: rotateY,
+      })
+    })
+
+    card.addEventListener('mouseleave', () => {
+      gsap.to(item, {
+        rotateX: 0,
+        rotateY: 0,
       })
     })
   })
